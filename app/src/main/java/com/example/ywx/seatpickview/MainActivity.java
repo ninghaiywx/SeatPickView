73371de7263bb12cfb78e7dbc133a96e3cb732ad
Lancer <416086226@qq.com>
2017-08-07 09:26:32 +0800

    增加缩略图 --v1.0.6

--- app/src/main/java/com/example/ywx/seatpickview/MainActivity.java
+++ app/src/main/java/com/example/ywx/seatpickview/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ywx.seatpickview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.ywx.mylibrary.SeatInfo;
 import com.example.ywx.mylibrary.SeatPickView;
@@ -11,12 +14,14 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SeatPickView pick;
+    private Button button;
     private List<SeatInfo>list=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pick=(SeatPickView)findViewById(R.id.pick);
+        button=(Button)findViewById(R.id.button);
         for(int i=0;i<10;i++){
                SeatInfo seatInfo=new SeatInfo();
                 seatInfo.setRow(0);
@@ -33,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
             seatInfo.setChoose(false);
             list.add(seatInfo);
         }
-        for(int i=0;i<15;i++){
+        for(int i=0;i<14;i++){
             SeatInfo seatInfo=new SeatInfo();
             seatInfo.setRow(2);
             seatInfo.setColums(i);
@@ -41,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             seatInfo.setChoose(false);
             list.add(seatInfo);
         }
-        for(int i=0;i<15;i++){
+        for(int i=0;i<14;i++){
             SeatInfo seatInfo=new SeatInfo();
             seatInfo.setRow(3);
             if(i==3){
@@ -53,6 +58,63 @@ public class MainActivity extends AppCompatActivity {
             seatInfo.setChoose(false);
             list.add(seatInfo);
         }
+        for(int i=0;i<10;i++){
+            SeatInfo seatInfo=new SeatInfo();
+            seatInfo.setRow(4);
+            seatInfo.setSeatstatue(0);
+            seatInfo.setColums(i);
+            seatInfo.setChoose(false);
+            list.add(seatInfo);
+        }
+        for(int i=0;i<10;i++){
+            SeatInfo seatInfo=new SeatInfo();
+            seatInfo.setRow(5);
+            seatInfo.setSeatstatue(0);
+            seatInfo.setColums(i);
+            seatInfo.setChoose(false);
+            list.add(seatInfo);
+        }
+        for(int i=0;i<10;i++){
+            SeatInfo seatInfo=new SeatInfo();
+            seatInfo.setRow(6);
+            seatInfo.setSeatstatue(0);
+            seatInfo.setColums(i);
+            seatInfo.setChoose(false);
+            list.add(seatInfo);
+        }
+        for(int i=0;i<12;i++){
+            SeatInfo seatInfo=new SeatInfo();
+            seatInfo.setRow(7);
+            seatInfo.setSeatstatue(0);
+            seatInfo.setColums(i);
+            seatInfo.setChoose(false);
+            list.add(seatInfo);
+        }
+        for(int i=0;i<12;i++){
+            SeatInfo seatInfo=new SeatInfo();
+            seatInfo.setRow(8);
+            seatInfo.setSeatstatue(0);
+            seatInfo.setColums(i);
+            seatInfo.setChoose(false);
+            list.add(seatInfo);
+        }
+        for(int i=0;i<12;i++){
+            SeatInfo seatInfo=new SeatInfo();
+            seatInfo.setRow(9);
+            seatInfo.setSeatstatue(0);
+            seatInfo.setColums(i);
+            seatInfo.setChoose(false);
+            list.add(seatInfo);
+        }
         pick.setSeatList(list);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(SeatInfo seatInfo:pick.getSelectedSeatList()){
+                    Toast.makeText(MainActivity.this,(seatInfo.getRow()+1)+","+(seatInfo.getColums()+1),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
